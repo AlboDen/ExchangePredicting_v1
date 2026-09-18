ed2fc556d7bf7bb544ea406ee873e3396844b843
Alex <lyakishev_i_vah@mail.ru>
2026-09-18 22:54:57 +0300

    finished adding a main widget

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # This is a sample Python script.
-import time
 
 import matplotlib.pyplot as plt
+
+from visual.graph import Graph
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from pybit.unified_trading import HTTP
+
+from visual.window import Window
+import time
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -19,10 +22,7 @@ def print_hi(name):
 
 
 if __name__ == '__main__':
-    plt.ion()
-    while(True):
-        print("start")
-
-        print("start")
-        time.sleep(30)
-        print("finish")
+    print("start")
+    app = Window()
+    app.mainloop()
+    print("finish")
